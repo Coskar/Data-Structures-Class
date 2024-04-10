82aee4139364e16e8361af7f835ad72873de6730
Coskar <joshuawall64@gmail.com>
2024-04-09 21:20:59 -0500

    Assignment 2: Binary search template function
    This lesson was all about Searching and Algorithm Analysis. In this assignment we first needed to implement the BinarySearch() function in the Searcher.h file. BinarySearch() performs a binary search on the sorted array (first parameter) for the key (third parameter). BinarySearch() returns the key's index if found, -1 if not found.
    Very simple binary search assignment.

--- DataStructures/Binary search template function/Searcher.h
+++ DataStructures/Binary search template function/Searcher.h
@@ -10,7 +10,26 @@ public:
    // found
    static int BinarySearch(T* array, int arraySize, const T& key,
       Comparer<T>& comparer) {
-      // Your code here (remove placeholder line below)
+      int left = 0;
+      int right = arraySize - 1;
+
+      while (left <= right) {
+         int mid = left + (right - left) / 2;
+
+         // Compare array[mid] with the key using the comparer
+         int comparisonResult = comparer.Compare(array[mid], key);
+
+         if (comparisonResult == 0) {
+            // Key found
+            return mid;
+         } else if (comparisonResult < 0) {
+            // If array[mid] < key, search the right half
+            left = mid + 1;
+         } else {
+            // If array[mid] > key, search the left half
+            right = mid - 1;
+         }
+      }
       return -1;
    }
 };
